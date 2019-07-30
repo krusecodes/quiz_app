a29ff0483c7cbfd7489c153f7c8ee0978b5adceb
Noah Kruse <noah.j.kruse@biola.edu>
2019-07-30 08:31:27 -0700

    tried two attempts of making the Submit button function properly

--- index.js
+++ index.js
@@ -53,11 +53,11 @@ function startQuiz() {
   function renderQuestion () {
     $('.questionForm').html(generateQuestion());
   }
+ 
+///////////////////////////////////////////////////////////////////  
 
-$(startQuiz)
-  
 //   function userSelectAnswer () {
-//     $('form').on('submit', function (event) {
+//     $('.questionForm').click(function(event){
 //       event.preventDefault();
 //       let selected = $('input:checked');
 //       let answer = selected.val();
@@ -71,3 +71,96 @@ $(startQuiz)
 //       }
 //     });
 //   }
+  
+//   function ifAnswerIsCorrect () {
+//     userAnswerFeedbackCorrect();
+//     score++;
+//   }
+  
+//   function ifAnswerIsWrong () {
+//     userAnswerFeedbackWrong();
+//   }
+
+//   function userAnswerFeedbackCorrect () {
+//     let correctAnswer = `${STORE[questionNumber].correctAnswer}`;
+//     $('.questionForm').html(`<div class="correctFeedback">
+//     <form>
+//     <fieldset>
+//     <legend><img src="https://static.thenounproject.com/png/474922-200.png" alt="thumbs up" type="thumb-icon"></legend>
+//     <h2>Correct!</h2>
+//     <button type="button" class="button">Next Question</button>
+//     <ul>
+//         <li>Questions: ${questionNumber}/5</li>
+//         <li> Correct: ${score}</li>
+//     </ul>
+//   </fieldset></form></div>`);
+//   }
+  
+//  function  userAnswerFeedbackWrong() {
+//   let correctAnswer = `${STORE[questionNumber].correctAnswer}`;
+//   $('.questionForm').html(`<div class="correctFeedback">
+//   <form>
+//   <fieldset>
+//   <legend><img src="https://static.thenounproject.com/png/1101097-200.png" alt="thumbs up" type="thumb-icon"></legend>
+//   <h2>Wrong answer! The correct answer is, <span>"${correctAnswer}"</span></h2>
+//   <button type="button" class="button">Next Question</button>
+//   <ul>
+//       <li>Questions: ${questionNumber}/5</li>
+//       <li> Correct: ${score}</li>
+//   </ul>
+// </fieldset>
+// </form>
+// </div>`);
+// }
+
+/////////////////////////////////////////////////////////////////
+  function userSelectAnswer () {
+    $(".questionform").on("submit", function (event) {
+    event.preventDefault();
+    let selected = $("input:checked");
+    let answer = selected.val();
+    let correctAnswer = `${STORE[qNum].correctAnswer}`;
+    if (answer === correctAnswer) {
+      return (<div class="correctAnswer-${questionNumber}"> 
+      <form>
+      <fieldset>
+        <legend>Correct!</legend>
+        <button type="button" class="button">Next Question</button>
+        <ul>
+            <li>Questions: ${score}/5</li>
+            <li> Correct: ${questionNumber}</li>
+        </ul>
+      </fieldset>
+    </form>
+    </div>)
+    score++
+    } else {
+      return (<div class='wrongAnswer-1'> 
+      <form>
+      <fieldset>
+        <legend>Wrong Answer</legend>
+        <h2>The correct answer is<span>${correctAnswer}</span></h2>
+        <button type="button" class="button">Next Question</button>
+        <ul>
+            <li>Questions: ${score}/5</li>
+            <li> Correct: ${questionNumber}</li>
+        </ul>
+      </fieldset>
+    </form>
+    </div>)
+    }
+  });
+}
+
+
+function ifAnswerIsCorrect () {
+  userAnswerFeedbackCorrect();
+  updateScore();
+}
+
+function ifAnswerIsWrong () {
+  userAnswerFeedbackWrong();
+}
+
+
+$(startQuiz)
