56b211972a390b51b8dd5c8a9bfda479332fd1d7
Noah Kruse <noah.j.kruse@biola.edu>
2019-08-04 09:48:49 -0700

    All functions accounted for. Quiz is not incrementing correctly

--- index.js
+++ index.js
@@ -1,61 +1,77 @@
-let questionNumber = 1;
+let questionNumber = 0;
 let score = 0;
 
-function generateQuestion () {
-  console.log("hello");
+function generateQuestion() {
   if (questionNumber < STORE.length) {
-      return `<div class="questionForm-${questionNumber}">
+    return `<div class="questionForm-${questionNumber}">
       <form class="quiz-form">
       <fieldset>
       <legend>${STORE[questionNumber].question}</legend>
       <label class="answerOption">
-      <input type="radio" value="${STORE[questionNumber].answer[0]}" name="answer" required>
+      <input type="radio" value="${
+      STORE[questionNumber].answer[0]
+      }" name="answer" required>
       <span>${STORE[questionNumber].answer[0]}</span>
       </label>
       <label class="answerOption">
-      <input type="radio" value="${STORE[questionNumber].answer[1]}" name="answer" required>
+      <input type="radio" value="${
+      STORE[questionNumber].answer[1]
+      }" name="answer" required>
       <span>${STORE[questionNumber].answer[1]}</span>
       </label>
       <label class="answerOption">
-      <input type="radio" value="${STORE[questionNumber].answer[2]}" name="answer" required>
+      <input type="radio" value="${
+      STORE[questionNumber].answer[2]
+      }" name="answer" required>
       <span>${STORE[questionNumber].answer[2]}</span>
       </label>
       <label class="answerOption">
-      <input type="radio" value="${STORE[questionNumber].answer[3]}" name="answer" required>
+      <input type="radio" value="${
+      STORE[questionNumber].answer[3]
+      }" name="answer" required>
       <span>${STORE[questionNumber].answer[3]}</span>
       </label>
-      <button type="submit" class="startButton">Submit</button>
+      <label class="answerOption">
+      <input type="radio" value="${
+      STORE[questionNumber].answer[4]
+      }" name="answer" required>
+      <span>${STORE[questionNumber].answer[4]}</span>
+      </label>
+      <input type="submit" id="submitButton" value="Submit" />
       <ul>
         <li>Questions: ${questionNumber}/5</li>
         <li>Correct: ${score}</li>
-      </ul> 
+      </ul>
       </fieldset>
       </form>
       </div>`;
-      questionNumber++
   } else {
-      renderResults();
-      restartQuiz();
-      $('.questionNumber').text(5)
-    }
+    renderResults();
+    restartQuiz();
+    $(".questionNumber").text(5);
   }
+}
+
+function changeQuestionNumber() {
+    questionNumber++;
+  $(".questionNumber").text(questionNumber+1);
+}
 
 function startQuiz() {
-    $('.startButton').click(function(event) {
-      console.log(startQuiz);
-      event.preventDefault();
-      $('.beginQuiz').remove();
-      $('.questionForm').css('display', 'block');
-      renderQuestion ();
+  questionNumber++;
+  $(".startButton").click(function (event) {
+    event.preventDefault();
+    $(".beginQuiz").remove();
+    $(".questionForm").css("display", "block");
+    renderQuestion();
   });
-  }
+}
 
-  function renderQuestion () {
-    $('.questionForm').html(generateQuestion());
-  }
- 
-///////////////////////////////////////////////////////////////////  
-
+function renderQuestion() {
+  $(".questionForm").html(generateQuestion());
+  userSelectAnswer();
+}
+///////////////////////////////////////////////////////////////////
 //   function userSelectAnswer () {
 //     $('.questionForm').click(function(event){
 //       event.preventDefault();
@@ -71,16 +87,13 @@ function startQuiz() {
 //       }
 //     });
 //   }
-  
 //   function ifAnswerIsCorrect () {
 //     userAnswerFeedbackCorrect();
 //     score++;
 //   }
-  
 //   function ifAnswerIsWrong () {
 //     userAnswerFeedbackWrong();
 //   }
-
 //   function userAnswerFeedbackCorrect () {
 //     let correctAnswer = `${STORE[questionNumber].correctAnswer}`;
 //     $('.questionForm').html(`<div class="correctFeedback">
@@ -95,7 +108,6 @@ function startQuiz() {
 //     </ul>
 //   </fieldset></form></div>`);
 //   }
-  
 //  function  userAnswerFeedbackWrong() {
 //   let correctAnswer = `${STORE[questionNumber].correctAnswer}`;
 //   $('.questionForm').html(`<div class="correctFeedback">
@@ -112,55 +124,105 @@ function startQuiz() {
 // </form>
 // </div>`);
 // }
-
 /////////////////////////////////////////////////////////////////
-  function userSelectAnswer () {
-    $(".questionform").on("submit", function (event) {
+function generateQuiz() {
+  startQuiz();
+  // nextQuest();
+}
+
+function correctQuizAnswer() {
+  score++;
+  return `<div class="correctAnswer-${questionNumber}">
+  <form>
+  <fieldset>
+    <legend>Correct!</legend>
+    <button type="button" class="nextbutton">Next Question</button>
+    <ul>
+        <li>Questions: ${questionNumber}/5</li>
+        <li> Correct: ${score}</li>
+    </ul>
+  </fieldset>
+</form>
+</div>`;
+}
+
+function wrongQuizAnswer() {
+  let correctAnswer = `${STORE[questionNumber].correctAnswer}`;
+  return `<div class="wrongAnswer"> 
+  <form>
+  <fieldset>
+    <legend>Wrong Answer</legend>
+    <h2>The correct answer is: <span>"${correctAnswer}"</span></h2>
+    <button type="button" class="nextButton">Next Question</button>
+    <ul>
+        <li>Questions: ${questionNumber}/5</li>
+        <li> Correct: ${score}</li>
+    </ul>
+  </fieldset>
+</form>
+</div>`;
+}
+
+function userSelectAnswer() {
+  let inputArrays = $(".answerOption").find("input[type=radio]");
+  console.log(inputArrays);
+  $("#submitButton").click(function (event) {
+    console.log("selectAnswer");
     event.preventDefault();
     let selected = $("input:checked");
     let answer = selected.val();
-    let correctAnswer = `${STORE[qNum].correctAnswer}`;
+    let correctAnswer = `${STORE[questionNumber].correctAnswer}`;
     if (answer === correctAnswer) {
-      return (<div class="correctAnswer-${questionNumber}"> 
-      <form>
-      <fieldset>
-        <legend>Correct!</legend>
-        <button type="button" class="button">Next Question</button>
-        <ul>
-            <li>Questions: ${score}/5</li>
-            <li> Correct: ${questionNumber}</li>
-        </ul>
-      </fieldset>
-    </form>
-    </div>)
-    score++
+      $(".quiz-form").html(correctQuizAnswer());
+      score++;
     } else {
-      return (<div class='wrongAnswer-1'> 
-      <form>
-      <fieldset>
-        <legend>Wrong Answer</legend>
-        <h2>The correct answer is<span>${correctAnswer}</span></h2>
-        <button type="button" class="button">Next Question</button>
-        <ul>
-            <li>Questions: ${score}/5</li>
-            <li> Correct: ${questionNumber}</li>
-        </ul>
-      </fieldset>
-    </form>
-    </div>)
+      console.log("supsup");
+      $(".quiz-form").html(wrongQuizAnswer());
     }
+    renderNextQuestion();
   });
 }
 
+function renderNextQuestion() {
+  console.log("cooolll");
+  $(".nextButton").click(function (event) {
+    event.preventDefault();
+    renderQuestion();
+    userSelectAnswer();
+    generateQuestion();
+    changeQuestionNumber();
+  });
+}
 
-function ifAnswerIsCorrect () {
+function renderResults(){
+  if (score = 5){
+    $('.questionForm').html(`<div class="resultsFeedback">
+    <h2>You're on fire!</h2>
+    <p>You got ${score} / 5</p><p>You're ready to swim with the fishes!</p>
+    <button class="restartButton">Restart Quiz</button>
+    </div>`);
+  }
+  else {
+    $('.questionForm').html(`<div class="resultsFeedback">
+    <h2>Almost There</h2>
+    <p>You got ${score} / 5</p><p>You're ready to swim with the fishes!</p>
+    <button class="restartButton">Restart Quiz</button>
+    </div>`);
+  }
+}
+
+function restartQuiz() {
+  console.log("cooolll");
+  $(".restartButton").click(function (event) {
+    location.reload();
+  });
+}
+
+function ifAnswerIsCorrect() {
   userAnswerFeedbackCorrect();
   updateScore();
 }
-
-function ifAnswerIsWrong () {
+function ifAnswerIsWrong() {
   userAnswerFeedbackWrong();
 }
-
-
-$(startQuiz)
+$(generateQuiz);
